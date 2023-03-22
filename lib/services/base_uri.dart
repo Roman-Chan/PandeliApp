781e18e10672aa4b882623ebeffac3c0abcbaba0
roman <roman.chan2622@gmail.com>
2023-03-22 08:03:00 -0600

    url

--- lib/services/base_uri.dart
+++ lib/services/base_uri.dart
@@ -1,3 +1,4 @@
 // const String baseUrl = 'http://192.168.137.56:3000';
 // const String baseUrl = 'http://192.168.100.10:3000';
-const String baseUrl = 'https://pandeliapi-production.up.railway.app';
+//const String baseUrl = 'https://pandeliapi-production.up.railway.app';
+const String baseUrl = 'https://pandeliapi-production-2d83.up.railway.app';
